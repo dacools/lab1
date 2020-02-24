f70c951c8df3b027f7ce80ebe5756c29ced2bd6f
Nathaniel Zollinger <nathaniel.zollinger@huskers.unl.edu>
2020-02-24 11:23:15 -0600

    fixed get_param error

--- scripts/pid_remote_drive.py
+++ scripts/pid_remote_drive.py
@@ -10,35 +10,34 @@ def turtle_msg(data, self):
     self.x = data.linear.x # set linear component
     self.z = data.angular.z # set angular component
 
-    distance = 100.0*self.distance_multiplier #set a distance offset
-    ang = 15.0*self.angle_multiplier #set the angle offest
+    dist = 100*self.distance_multiplier # set a distance offset
+    ang = 15*self.angle_multiplier # set the angle offest
 
     # retrieve the target parameters
-    dist_target_left = rospy.get_param("distance/tar/left")
-    dist_target_right = rospy.get_param("distance/tar/right")
-    angle_target = rospy.get_param("angle/target")
+    self.left = rospy.get_param("distance/tar/left")
+    self.right = rospy.get_param("distance/tar/right")
+    self.angle = rospy.get_param("angle/target")
 
     if self.x > 0:
         # move forward
-        self.left = dist_target_left + distance
-        self.right = dist_target_right + distance
+        self.left = self.left + dist
+        self.right = self.right + dist
     elif self.x < 0:
         # move backward
-        self.left = dist_target_left - distance
-        self.right = dist_target_right - distance
+        self.left = self.left - dist
+        self.right = self.right - dist
     elif self.z > 0:
         # turn left
-        self.angle = angle_target - ang
+        self.angle = self.angle - ang
     elif self.z < 0:
         # turn right
-        self.angle = angle_target + ang
+        self.angle = self.angle + ang
     else:
         rospy.loginfo("Parse error")
 
     rospy.set_param("distance/tar/left",self.left)
     rospy.set_param("distance/tar/right",self.right)
     rospy.set_param("angle/target",self.angle)
-
 
 class TheNode(object):
   # This class holds the rospy logic for updating the PID targets
@@ -48,16 +47,15 @@ class TheNode(object):
 
     rospy.init_node('pid_remote_drive') # intialize node
 
-    # self.msg_data = Twist() # Twist variable for message received
-    self.x = 0 # variable for linear component
-    self.z = 0 # variable for angular component
+    self.x = 0 # linear component variable
+    self.z = 0 # angular component variable
 
     self.left = 0 # left distance variable
     self.right = 0 # right distance variable
     self.angle = 0 # angle variable
 
-    self.angle_multiplier = 1 # variable for the angle multiplier
-    self.distance_multiplier = 1 # variable for the distance multiplier
+    self.angle_multiplier = 1 # angle multiplier variable
+    self.distance_multiplier = 1 # distance multiplier variable
 
   def main_loop(self):
     # initialize subscriber node for messages from teleop_turtle_key
